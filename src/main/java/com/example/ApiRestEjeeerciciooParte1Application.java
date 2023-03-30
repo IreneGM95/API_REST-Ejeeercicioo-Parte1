be975a8f78a75e08d6a1da4315e9d7ea17886bbb
IreneG <irene.gonza.mor@gmail.com>
2023-03-30 17:11:49 +0200

    interfaz implementada en Appp

--- src/main/java/com/example/ApiRestEjeeerciciooParte1Application.java
+++ src/main/java/com/example/ApiRestEjeeerciciooParte1Application.java
@@ -1,13 +1,20 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestEjeeerciciooParte1Application {
+public class ApiRestEjeeerciciooParte1Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestEjeeerciciooParte1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'run'");
 	}
 
 }
