afb7e0db3ce58ef82952c1b6fe8a3c965d964ee4
IreneG <irene.gonza.mor@gmail.com>
2023-03-30 14:55:33 +0200

    Estado inicial

---
+++ src/main/java/com/example/ApiRestEjeeerciciooParte1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEjeeerciciooParte1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEjeeerciciooParte1Application.class, args);
+	}
+
+}
